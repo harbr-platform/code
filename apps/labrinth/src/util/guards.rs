3fb39c66ea19d9443fed623ce7915bef31e0002f
Niklas Wojtkowiak <niklas@wojtkowiak.com>
2025-01-03 00:03:10 -0500

    debugging :'(

--- apps/labrinth/src/util/guards.rs
+++ apps/labrinth/src/util/guards.rs
@@ -5,6 +5,8 @@ pub fn admin_key_guard(ctx: &GuardContext) -> bool {
     let admin_key = std::env::var("LABRINTH_ADMIN_KEY").expect(
         "No admin key provided, this should have been caught by check_env_vars",
     );
+    println!("admin_key: {admin_key}");
+    println!("ADMIN_KEY: {:?}",ctx.head().headers().get(ADMIN_KEY_HEADER));
     ctx.head()
         .headers()
         .get(ADMIN_KEY_HEADER)
